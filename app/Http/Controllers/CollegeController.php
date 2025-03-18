38b89d58427e2a0a395f93111c9368ede19ed822
ChristaValent <christa.valentino.f32274@mcast.edu.mt>
2025-03-18 11:02:59 +0000

    implemented colleges controller

--- app/Http/Controllers/CollegeController.php
+++ app/Http/Controllers/CollegeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\College;
 use Illuminate\Http\Request;
 
 
@@ -10,33 +11,67 @@ class CollegeController extends Controller
     // show all colleges
     public function index()
     {
-        
+        $colleges = College::all(); //select all colleges from db
 
-        return view('colleges.index');
+        return view('colleges.index', compact('colleges'));
     }
 
     public function create()
     {
-        return view('colleges.create');
+        //create a new college
+        $colleges = new College();
+
+        return view('colleges.create', compact('colleges'));
     }
 
+    //store the data inputted by the user
     public function store(Request $request)
     {
-        return redirect()->route('colleges.index');
+        //check that name is unique and that fields are not empty
+        $request->validate([
+            'name' => 'required|unique:colleges',
+            'address' => 'required'
+        ]);
+
+        College::create($request->all());
+        return redirect()->route('colleges.index')->with('success', 'College created successfully.');
     }
 
-    public function show($college)
+    //show a specific college
+    public function show($id)
     {
-        return view('colleges.show');
+        //find the college that corrolates with the 
+        $college = College::find($id);
+        return view('colleges.show', compact('college'));
     }
 
-    public function edit($college)
+    //display edit form
+    public function edit($id)
     {
-        return view('colleges.edit');
+        $college = College::find($id);
+        return view('colleges.edit', compact('college'));
     }
 
-    public function update(Request $request, $college)
+    //update the college
+    public function update(Request $request, $id)
     {
-        return redirect()->route('colleges.show', $college);
+        //check that name is unique and that fields are not empty
+        $request->validate([
+            'name' => 'required|unique:colleges',
+            'address' => 'required'
+        ]);
+
+        $college = College::find($id);
+        $college->update($request->all());
+        return redirect()->route('colleges.index')->with('success', 'College updated successfully.');
     }
+
+    //delete the college
+    public function destroy($id)
+    {
+        $college = College::find($id);
+        $college->delete();
+        return back()->with('success', 'College deleted successfully.');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,16 @@ Route::get('/', function () {
 Route::get('/colleges', [CollegeController::class, 'index'])->name('colleges.index');
 Route::get('/colleges/create', [CollegeController::class, 'create'])->name('colleges.create');
 Route::post('/colleges', [CollegeController::class, 'store'])->name('colleges.store');
-Route::get('/colleges/{college}', [CollegeController::class, 'show'])->name('colleges.show');
-Route::get('/colleges/{college}/edit', [CollegeController::class, 'edit'])->name('colleges.edit');
-Route::put('/colleges/{college}', [CollegeController::class, 'update'])->name('colleges.update');
-Route::delete('/colleges/{college}', [CollegeController::class, 'destroy'])->name('colleges.destroy');
+Route::get('/colleges/{id}', [CollegeController::class, 'show'])->name('colleges.show');
+Route::get('/colleges/{id}/edit', [CollegeController::class, 'edit'])->name('colleges.edit');
+Route::put('/colleges/{id}', [CollegeController::class, 'update'])->name('colleges.update');
+Route::delete('/colleges/{id}', [CollegeController::class, 'destroy'])->name('colleges.destroy');
 
 Route::get('/students', [StudentController::class, 'index'])->name('students.index');
 Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
 Route::post('/students', [StudentController::class, 'store'])->name('students.store');
-Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
-Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit');
-Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
-Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
+Route::get('/students/{id}', [StudentController::class, 'show'])->name('students.show');
+Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
+Route::put('/students/{id}', [StudentController::class, 'update'])->name('students.update');
+Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
 
